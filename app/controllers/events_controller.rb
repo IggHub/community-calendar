44fd6607385cc1ca3f4e21e7e2c77b4419bdfa1f
Iggy <igoririanto@rocketmail.com>
2017-11-21 22:03:26 -0800

    rails generate events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,21 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
